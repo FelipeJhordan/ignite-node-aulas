31928f4ee9a0c41bb27faaa3e64d698b3a0c8a2b
felipejhordan <felipejordan.alves@gmail.com>
2022-05-08 22:42:46 -0300

    feat(finapi): implement first business rule and finish the route for create a account

--- chapter-1/finapi/src/index.js
+++ chapter-1/finapi/src/index.js
@@ -1,5 +1,38 @@
 const express = require("express")
+const { randomUUID } = require('crypto')
+
 
 const app = express()
+
+app.use(express.json())
+
+const customers = []
+
+app.post("/account", (request, response) => {
+    const { cpf, name } = request.body
+
+    const customerAlreadyExist = customers.some(
+        (customer) => customer.cpf === cpf
+    )
+
+    if(customerAlreadyExist) {
+        return response.status(400).json({
+            error: "Customer already exists!"
+        })
+    }
+
+    
+    customers.push({
+        cpf,
+        name,
+        id: randomUUID(),
+        statement: []
+    })
+
+
+    console.log(customers)
+
+    return response.send()
+})
 
 app.listen(3333)
